d1efaf6e75f3145ee94c7fc33cebe37b15c895c3
cwb <sy_caowubin@aliyun.com>
2019-06-11 17:09:44 +0800

    feat:添加products.json,使用fetch模拟HttpRequest

--- src/containers/FilterableProductTable.js
+++ src/containers/FilterableProductTable.js
@@ -5,20 +5,12 @@ import ProductTable from './ProductTable';
 import SearchBar from './SearchBar';
 import './FilterableProductTable.css';
 
-const data = [
-  {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
-  {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
-  {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
-  {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
-  {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
-  {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
-];
-
 export default class FilterableProductTable extends Component {
   constructor (props) {
     super(props);
     this.state = {
-      products: data
+      sourceProducts: [],
+      products: []
     }
     this.search = this.search.bind(this);
   }
@@ -26,10 +18,25 @@ export default class FilterableProductTable extends Component {
   search ({searchTxt, onlyInStock}) {
     const lowerSearchTxt = searchTxt.toLowerCase()
     this.setState({
-      products: data.filter(product => 
+      products: this.state.sourceProducts.filter(product => 
         (product.category.toLowerCase().indexOf(lowerSearchTxt) > -1 || product.name.toLowerCase().indexOf(lowerSearchTxt) > -1) && (!onlyInStock || product.stocked)
       )
     })
+  }
+
+  componentDidMount() {
+    fetch('./products.json').then(response => {
+        return response.json()
+      })
+      .then(data => {
+        this.setState({
+          sourceProducts: data.products,
+          products: data.products
+        })
+        this.search()
+      }).catch(err => {
+        console.log('Error Reading data ' + err);
+      })
   }
 
   render() {
@@ -64,4 +71,5 @@ export default class FilterableProductTable extends Component {
       </div>
     );
   }
+
 }
