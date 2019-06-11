6a8e55b4958763abc01df7d3961d0efeddebbde0
cwb <sy_caowubin@aliyun.com>
2019-06-11 12:28:53 +0800

    Updates

---
+++ precache-manifest.eb49df9bd1957189dc57d8bc26fc9f13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0237bfe74e5db8b739c3fdce3e5a3baf",
+    "url": "/react-learn-app/index.html"
+  },
+  {
+    "revision": "97383a93fdab7274d59e",
+    "url": "/react-learn-app/static/css/main.bbafeb6a.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/react-learn-app/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "97383a93fdab7274d59e",
+    "url": "/react-learn-app/static/js/main.be6cffd6.chunk.js"
+  },
+  {
+    "revision": "c5762b5a4b9bc68ebc49",
+    "url": "/react-learn-app/static/js/runtime~main.0374a5c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-learn-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
