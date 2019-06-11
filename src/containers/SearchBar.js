4af7058f02fb115dd5b7db79d5154a14a7bc0926
cwb <sy_caowubin@aliyun.com>
2019-06-11 12:53:54 +0800

    docs: add lifecycle in src/react-docs
    Signed-off-by: cwb <sy_caowubin@aliyun.com>

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -8,19 +8,35 @@ export default class ProductCategoryRow extends Component {
       searchTxt: '',
       onlyInStock: false
     };
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
   render() {
     return (
       <div className="search-Bar">
+        <pre>{JSON.stringify(this.state)}</pre>
         <div className="search-Bar__input">
-          <input type="text" />
+          <input
+            name="searchTxt"
+            type="text"
+            value={this.state.searchTxt}
+            onChange={this.handleInputChange} />
         </div>
         <div className="search-Bar__additional">
           <input
-            name="isGoing"
-            type="radio"
-            value={this.state.onlyInStock} />
+            name="onlyInStock"
+            type="checkbox"
+            value={this.state.onlyInStock}
+            onChange={this.handleInputChange} />
           <span>Only show products in stock</span>
         </div>
       </div>
---
+++ src/react-docs/lifecycle/lifecycle.md
@@ -0,0 +1,3 @@
+# react-lifecycle
+
+![](./lifecycle.jpeg)
