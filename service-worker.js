35dad3379448586c331443bd4b2f088dd45833f6
xiaobaifeng <835277213@qq.com>
2019-10-15 18:03:50 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-learn-app/precache-manifest.959b34d205fb9edbe5dd789d7fe42a19.js"
+  "/precache-manifest.e9795a0ab7c0d0c8bf6a1f1c01f244e0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-learn-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
